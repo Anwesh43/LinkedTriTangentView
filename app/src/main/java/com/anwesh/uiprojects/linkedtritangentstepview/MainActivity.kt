3250969eefca607256622eeeb4a48b27adc0a8e1
Anwesh43 <anweshthecool0@gmail.com>
2019-01-02 09:36:41 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtritangentstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtritangentstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtritangentstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.tritangentstepview.TriTangentStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriTangentStepView.create(this)
     }
 }
