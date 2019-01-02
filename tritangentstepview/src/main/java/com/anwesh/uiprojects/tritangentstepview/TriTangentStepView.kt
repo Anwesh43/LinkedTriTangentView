2dc6d88f324ab68581f8a7885c4651e6b2320eac
Anwesh43 <anweshthecool0@gmail.com>
2019-01-02 08:58:01 +0530

    adding Float and Int extension methods

--- tritangentstepview/src/main/java/com/anwesh/uiprojects/tritangentstepview/TriTangentStepView.kt
+++ tritangentstepview/src/main/java/com/anwesh/uiprojects/tritangentstepview/TriTangentStepView.kt
@@ -21,3 +21,10 @@ val strokeFactor : Int = 90
 val sizeFactor : Int = 3
 val foreColor : Int = Color.parseColor("#283593")
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * scGap * dir
