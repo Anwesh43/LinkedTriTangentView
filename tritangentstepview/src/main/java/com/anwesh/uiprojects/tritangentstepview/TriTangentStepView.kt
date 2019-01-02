f8ae4bf53dc51666d57d34e457ffdd958c43d268
Anwesh43 <anweshthecool0@gmail.com>
2019-01-02 09:18:52 +0530

    adding TTSNode class

--- tritangentstepview/src/main/java/com/anwesh/uiprojects/tritangentstepview/TriTangentStepView.kt
+++ tritangentstepview/src/main/java/com/anwesh/uiprojects/tritangentstepview/TriTangentStepView.kt
@@ -138,6 +138,48 @@ class TriTangentStepView(ctx : Context) : View(ctx) {
                 animated = false
             }
         }
+    }
 
+    data class TTSNode(var i : Int, val state : State = State()) {
+        private var prev : TTSNode? = null
+        private var next : TTSNode? = prev
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TTSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTTSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TTSNode {
+            var curr : TTSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
     }
 }
