996776e326ee19f2131c4d857453784b0ab4d66c
Anwesh43 <anweshthecool0@gmail.com>
2019-01-02 08:53:35 +0530

    adding libraries and adding constants

--- tritangentstepview/src/main/java/com/anwesh/uiprojects/tritangentstepview/TriTangentStepView.kt
+++ tritangentstepview/src/main/java/com/anwesh/uiprojects/tritangentstepview/TriTangentStepView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.tritangentstepview
 /**
  * Created by anweshmishra on 02/01/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Path
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Int = 3
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
