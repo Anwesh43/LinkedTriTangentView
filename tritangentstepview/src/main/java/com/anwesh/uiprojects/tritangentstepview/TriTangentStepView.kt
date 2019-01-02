fd91a05a17a63d7141757a4f19deeb2804f2aeb6
Anwesh43 <anweshthecool0@gmail.com>
2019-01-02 09:11:19 +0530

    adding TriTangentStepView class

--- tritangentstepview/src/main/java/com/anwesh/uiprojects/tritangentstepview/TriTangentStepView.kt
+++ tritangentstepview/src/main/java/com/anwesh/uiprojects/tritangentstepview/TriTangentStepView.kt
@@ -74,3 +74,21 @@ fun Canvas.drawTTSNode(i : Int, scale : Float, paint : Paint) {
     drawTangent(size, scale, paint)
     restore()
 }
+
+class TriTangentStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
